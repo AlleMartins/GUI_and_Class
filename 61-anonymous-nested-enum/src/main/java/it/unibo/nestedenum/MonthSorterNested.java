7c428b1035169be5d0de2e6aa63690fe48d46177
Alessandro Martini <alessandro.martini10@studio.unibo.it>
2022-11-10 15:41:25 +0100

    Add all file, ex 61 complete

--- 61-anonymous-nested-enum/src/main/java/it/unibo/nestedenum/MonthSorterNested.java
+++ 61-anonymous-nested-enum/src/main/java/it/unibo/nestedenum/MonthSorterNested.java
@@ -9,13 +9,70 @@ import java.util.Objects;
  */
 public final class MonthSorterNested implements MonthSorter {
 
+    public enum Month {
+        JANUARY(31),
+        FEBRUARY(28),
+        MARCH(31),
+        APRIL(30),
+        MAY(31),
+        JUNE(30),
+        JULY(31),
+        AUGUST(31),
+        SEPTEMBER(30),
+        OCTOBER(31),
+        NOVEMBER(30),
+        DECEMBER(31);
+
+        private int days;
+
+        private Month(final int days){
+            this.days = days;
+        }
+
+        public static Month fromString(final String month){
+            Month result = null;
+            for(Month elem : Month.values()){
+                if(elem.toString().toLowerCase().startsWith( month.toString().toLowerCase())){
+                    if(result != null){
+                        throw new IllegalArgumentException();
+                    }
+                    result = elem;
+                }
+            }
+            if(result == null){
+                throw new IllegalArgumentException();
+            }
+            return result;
+        }
+    }
+
+    private class SortByDays implements Comparator<String>{
+        @Override
+        public int compare(String o1, String o2) {
+            final var var1 = Month.fromString(o1).days;
+            final var var2 = Month.fromString(o2).days;
+
+            return Integer.compare(var1, var2);
+        }     
+    }   
+
     @Override
     public Comparator<String> sortByDays() {
-        return null;
+        return new SortByDays();
+    }
+
+    private class SortByOrder implements Comparator<String>{
+        @Override
+        public int compare(String o1, String o2) {
+            final var var1 = Month.fromString(o1).ordinal();
+            final var var2 = Month.fromString(o2).ordinal();
+
+            return Integer.compare(var1, var2);
+        }
     }
 
     @Override
     public Comparator<String> sortByOrder() {
-        return null;
+        return new SortByOrder();
     }
 }
